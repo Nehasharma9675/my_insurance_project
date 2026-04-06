41481ccd5c3d0acfef727ec090bee2e53658adea
osndl.nehas <osndl.nehas@aicofindia.com>
2026-04-06 16:10:37 +0530

    fix requirements for deployment1

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import *
-urlpatterns = [
-     path("login",login_view, name="login"),
-     path("logout",logout_view, name="logout"),
+from .views import login_view, logout_view
 
-     
- ]
+urlpatterns = [
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+]
  

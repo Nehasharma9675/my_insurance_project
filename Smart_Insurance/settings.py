4e566085a9cd53b971726494252d26cef11db5bb
osndl.nehas <osndl.nehas@aicofindia.com>
2026-04-06 15:57:04 +0530

    fix requirements for deployment1

--- Smart_Insurance/settings.py
+++ Smart_Insurance/settings.py
@@ -88,14 +88,21 @@ CACHES = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'SMART_INSURANCE',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'SMART_INSURANCE',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

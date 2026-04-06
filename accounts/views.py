dc9bf5be176368029daca457deec9e3b8228d5b9
osndl.nehas <osndl.nehas@aicofindia.com>
2026-04-06 16:40:51 +0530

    fix role error

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
-import redis
+# import redis
 import time
 
 MAX_ATTEMPTS = 5
